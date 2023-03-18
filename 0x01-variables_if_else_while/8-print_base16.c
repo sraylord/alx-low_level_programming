da88fe24290e5ec83ce8bb6ee20ce4d6eb298cc1
oussama <olaouin@gmail.com>
2023-03-17 17:56:32 -0700

    ninth

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Entry Point
+ *Description: 'print numbers of base 16'
+ *Return: Always 10
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
