cf9abd23381e5013d657b97346dc120bbe6d5e55
oussama <olaouin@gmail.com>
2023-03-21 19:33:59 -0700

    second

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ *main - Entry point
+ *print_alphabet - utilizes on the _putchar function to print
+ *		  the alphabet a-z
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
+
