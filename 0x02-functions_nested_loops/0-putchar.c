c2d3a251769f11cc297d631f26fb7e4dab9bbc3d
oussama <olaouin@gmail.com>
2023-03-21 19:27:23 -0700

    first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'prints _putchar to stdout'
+ * Return: always 0
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
