ea3fe76c20f40850b9e4e18b1f7f57f28acf0bdc
oussama <olaouin@gmail.com>
2023-03-27 17:45:42 -0700

    second 2

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * swap_int -  swaps the values of two integers 
+ * swap_int -  swaps the values of two integers
  * using two input parameters
- * 
+ *
  * @a: input parameter 1
  * @b: input parameter 2
  * Return: Nothing
