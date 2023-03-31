97d3cc6cc2b488a59081f2a3d49e639bea7e4fba
oussama <olaouin@gmail.com>
2023-03-30 19:10:43 -0700

    sven

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *leet - encodes a string into 1337
+ * @c: String
+ * Return: string that is encoded
+*/
+
+char *leet(char *c)
+{
+	char *cp = cp;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			/*32 is the difference between lowercase letters and uppercase letters*/
+			if (*c == key[i] || *c == key[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
+		}
+		c++;
+	}
+	return (cp);
+}
