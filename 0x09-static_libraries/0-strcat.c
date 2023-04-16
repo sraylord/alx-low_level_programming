a45637db227a2617b8804f954fd37ca2f96e4352
oussama <olaouin@gmail.com>
2023-04-15 19:28:57 -0700

    libmy

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *
+ * @dest: pointer to destination input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int c, c2;
+
+	c = 0;
+	/*find the size of test array*/
+	while (dest[c])
+		c++;
+
+	/* iterate through each str array value without the null buyte */
+	for (c2 = 0; src[c2] ; c2++)
+		/* append src[c2] to dest[c] while overwriting the null byte in dest */
+		dest[c++] = src[c2];
+
+	return (dest);
+}
